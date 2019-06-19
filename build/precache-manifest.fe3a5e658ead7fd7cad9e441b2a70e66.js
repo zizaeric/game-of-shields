ec1e3f037094cc85ac883fcc7f045e2627400d46
zizaeric <zizaeric3@gmail.com>
2019-06-19 10:09:07 -0400

    last commit after running git run deploy

---
+++ build/precache-manifest.fe3a5e658ead7fd7cad9e441b2a70e66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7959203bb24ee4704e7b676b0b605ae8",
+    "url": "/game-of-shields/index.html"
+  },
+  {
+    "revision": "ee593b8a749daaee70d2",
+    "url": "/game-of-shields/static/css/main.46cfd664.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/game-of-shields/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "ee593b8a749daaee70d2",
+    "url": "/game-of-shields/static/js/main.930bb457.chunk.js"
+  },
+  {
+    "revision": "ec92d1d5925c4a311809",
+    "url": "/game-of-shields/static/js/runtime~main.62827afe.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Clicky-Game/precache-manifest.61e9a3e975a386f5900b38e662ccfc0c.js"
+  "/game-of-shields/precache-manifest.fe3a5e658ead7fd7cad9e441b2a70e66.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Clicky-Game/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/game-of-shields/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
